3f634ac242a367780614c77ee7403c5b1a67ab98
Dima <dm.knz@yandex.ru>
2020-03-30 22:56:39 +0200

    I dont know how export fuction in whis execise .

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+const readlineSync =  require('readline-sync');
+
+// Wait for user's res{
+
+ let userName = readlineSync.question('May I have your name? ');
+ console.log('Hi ' + userName + '!')
+
+
