9e09d6faea4e6e07f35a187b816c47681f3ce898
Dima <dm.knz@yandex.ru>
2020-03-31 21:08:50 +0200

    3rd exes

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-//import {function1} from './src/index.js';
+import {userName} from '../src/index.js';
 
 
 console.log ('Welcome to the Brain Games!');
+
+
 
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 
-const readlineSync =  require('readline-sync');
-
+//const readlineSync =  require('readline-sync');
+import  readlineSync from 'readline-sync'
 // Wait for user's res{
 
- let userName = readlineSync.question('May I have your name? ');
- console.log('Hi ' + userName + '!')
-
-
+//const function1  = () => {
+ export const userName = readlineSync.question('May I have your name? ');
+ console.log('Hi ' + userName + '!');
+//};
