1c7f8cd76172362637280f7568c4173c7f4be393
Dima <dm.knz@yandex.ru>
2020-04-01 20:21:38 +0200

    3rd exes

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,20 @@
+
 module.exports = {
     presets: [
-        ['@babel/env', {
-            targets: {
-                node: 'current',
+        ['@babel/preset-env',
+            {
+                targets: {
+                    node: 'current',
+                },
             },
-        }],
+        ],
+    ],
+    plugins: [
+        [
+            '@babel/plugin-proposal-pipeline-operator',
+            {
+                proposal: 'minimal',
+            },
+        ],
     ],
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-
-//const readlineSync =  require('readline-sync');
 import  readlineSync from 'readline-sync'
-// Wait for user's res{
 
-//const function1  = () => {
+
  export const userName = readlineSync.question('May I have your name? ');
  console.log('Hi ' + userName + '!');
-//};
+
