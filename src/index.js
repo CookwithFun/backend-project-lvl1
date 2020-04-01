8ff4b5a581d09c155abdb8aac6ca0dc6a04a7f52
Dima <dm.knz@yandex.ru>
2020-04-02 01:17:52 +0200

    3rd exes las

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import  readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync'
 
 
- export const userName = readlineSync.question('May I have your name? ');
- console.log('Hi ' + userName + '!');
+export const userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
 
